980626e659d2eef7678d4c120ee75812060497bf
Shubham Singh <shubhambaba143143@gmail.com>
2022-07-09 00:34:22 +0530

    fifth

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "./Emoji";
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">
+      {emojipedia.map(emoji=>
+   <Entry 
+        key={emoji.id}
+        emoji={emoji.emoji}
+        name={emoji.name}
+        meaning={emoji.meaning}
+       />
+      
+      )
+}
+      </dl>
+    </div>
+  );
+}
+
+export default App;
